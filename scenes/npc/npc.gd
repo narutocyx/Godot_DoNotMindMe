cf03afb664ce5f479acf626c51d025dc60450f1b
narutocyx <narutocyxlive@163.com>
2024-11-08 12:34:45 +0800

    Game 7 - 281.10

--- scenes/npc/npc.gd
+++ scenes/npc/npc.gd
@@ -2,16 +2,20 @@ extends CharacterBody2D
 
 const SPEED: float = 160.0
 
+enum ENEMY_STATE { PATROLLING, CHASING, SEARCHING }
+
 @export var patrol_points: NodePath
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var label: Label = $Label
 @onready var nav_agent: NavigationAgent2D = $NavAgent
 @onready var player_detect: Node2D = $PlayerDetect
+@onready var ray_cast_2d: RayCast2D = $PlayerDetect/RayCast2D
 
 var _waypoints: Array = []
 var _current_wp: int = 0
 var _player_ref: Player
+var _state: ENEMY_STATE = ENEMY_STATE.PATROLLING
  
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,24 +30,38 @@ func create_wp() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("set_target"):
-		nav_agent.target_position = get_global_mouse_position()
-	update_navigation()
-	process_patrolling()
+	#if Input.is_action_just_pressed("set_target"):
+		#nav_agent.target_position = get_global_mouse_position()
 	raycast_to_player()
-	set_label()
+	update_state()
+	update_movement()
+	update_navigation()
+	set_label() 
+
+#raycast
+func can_see_player() -> bool:
+	return player_in_fov() and player_detected()
+
+func get_fov_angle() -> float:
+	var direction = global_position.direction_to(_player_ref.global_position)
+	var dot_p = direction.dot(velocity.normalized())
+	if dot_p >= -1.0 and dot_p <= 1.0:
+		return rad_to_deg(acos(dot_p))
+	return 0.0
+	
+func player_in_fov() -> bool:
+	return get_fov_angle() < 60.0
 	
 func raycast_to_player() -> void:
 	player_detect.look_at(_player_ref.global_position)
 	
-func set_label():
-	var s: String = ""
-	s += "DONE:%s\n" % nav_agent.is_navigation_finished()
-	s += "REACHABLE:%s\n" % nav_agent.is_target_reachable()
-	s += "REACHED:%s\n" % nav_agent.is_target_reached()
-	s += "TARGET:%s" %nav_agent.target_position
-	label.text = s
+func player_detected() -> bool:
+	var c = ray_cast_2d.get_collider()
+	if c != null:
+		return c.is_in_group("player")
+	return false
 	
+#navigation
 func update_navigation() -> void:
 	if nav_agent.is_navigation_finished() == false:
 		var next_path_position: Vector2 = nav_agent.get_next_path_position()
@@ -53,7 +71,31 @@ func update_navigation() -> void:
 		velocity = global_position.direction_to(next_path_position) * SPEED
 		#移动
 		move_and_slide()
-		
+	
+#state
+func update_state() -> void:
+	var new_state = _state
+	var can_see = can_see_player()
+	if can_see == true:
+		new_state = ENEMY_STATE.CHASING
+	else:
+		new_state = ENEMY_STATE.PATROLLING
+	
+	set_state(new_state)
+	
+func set_state(new_state: ENEMY_STATE) -> void:
+	if new_state == _state:
+		return
+	_state = new_state
+
+#movement
+func update_movement() -> void:
+	match _state:
+		ENEMY_STATE.PATROLLING:
+			process_patrolling()
+		ENEMY_STATE.CHASING:
+			process_chasing()
+			
 func process_patrolling() -> void:
 	if nav_agent.is_navigation_finished() == true:
 		navigate_wp()
@@ -63,3 +105,19 @@ func navigate_wp() -> void:
 		_current_wp = 0
 	nav_agent.target_position = _waypoints[_current_wp]
 	_current_wp += 1
+	
+func process_chasing() -> void:
+	set_nav_to_player()
+	
+func set_nav_to_player() -> void:
+	nav_agent.target_position = _player_ref.global_position
+
+#debug
+func set_label():
+	var s: String = ""
+	s += "Done:%s\n" % nav_agent.is_navigation_finished()
+	s += "Reached:%s\n" % nav_agent.is_target_reached()
+	s += "Target:%s\n" % nav_agent.target_position
+	s += "PlayerDetected:%s\n" % player_detected()
+	s += "FOV:%.2f %s\n" % [get_fov_angle(), ENEMY_STATE.keys()[_state]]
+	label.text = s
